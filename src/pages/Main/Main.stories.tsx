1cc21acac15300e832c9a8f4daf3b3e6cfcec233
offdutyby <offdutybyblo@gmail.com>
2022-06-30 21:31:13 +0900

    feat: first commit for react project

---
+++ src/pages/Main/Main.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react'
+
+import Main from './Main'
+
+export default {
+  title: 'Pages/MainPage',
+  component: Main,
+} as Meta
+
+export const Default: Story = () => <Main />
