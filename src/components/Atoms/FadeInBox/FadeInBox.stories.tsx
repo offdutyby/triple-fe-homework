c75e3dffc073c3e2907c647cdc83cb456a193004
offdutyby <offdutybyblo@gmail.com>
2022-07-05 21:26:36 +0900

    feat: add args on stories

--- src/components/Atoms/FadeInBox/FadeInBox.stories.tsx
+++ src/components/Atoms/FadeInBox/FadeInBox.stories.tsx
@@ -1,14 +1,25 @@
 import { Meta, Story } from '@storybook/react'
+import { ReactNode } from 'react'
 
 import FadeInBox from './FadeInBox'
+
+interface Props {
+  children: ReactNode
+  delay: number
+}
 
 export default {
   title: 'components/Atoms/FadeInBox',
   component: FadeInBox,
 } as Meta
 
-function Template() {
-  return <FadeInBox delay={100}>안녕하세요</FadeInBox>
+function Template({ children, delay }: Props) {
+  return <FadeInBox delay={delay}>{children}</FadeInBox>
 }
 
-export const Default: Story = Template.bind({})
+export const Default: Story<Props> = Template.bind({})
+
+Default.args = {
+  children: <span>안녕하세요. 천천히 올라와요.</span>,
+  delay: 100,
+}
